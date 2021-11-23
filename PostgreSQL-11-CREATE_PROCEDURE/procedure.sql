72195cbffaa4484404aebcf432cc2c88d50fc719
Marcin Nowak <marcin.nowak@techmetria.pl>
2021-11-23 20:06:54 +0100

    PostgreSQL 11 CREATE PROCEDURE

--- PostgreSQL-11-CREATE_PROCEDURE/procedure.sql
+++ PostgreSQL-11-CREATE_PROCEDURE/procedure.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS accounts;
+
 create table if not exists accounts
 (
     active       boolean,
@@ -13,18 +15,46 @@ INSERT INTO accounts
 SELECT true, i, 'savings'
 FROM generate_series(-100, 100) s(i);
 
-CREATE OR REPLACE PROCEDURE deactivate_unpaid_accounts()
+CREATE OR REPLACE PROCEDURE deactivate_accounts_below_balance(balance_threshold decimal)
     LANGUAGE SQL
 AS
 $$
 
 UPDATE accounts
 SET active = false
-WHERE balance < 0;
+WHERE balance < balance_threshold;
 
 $$;
 
-CALL deactivate_unpaid_accounts();
+SELECT COUNT(*)
+FROM accounts
+WHERE active;
 
+CALL deactivate_accounts_below_balance(1);
 
-DROP TABLE IF EXISTS accounts;
+SELECT COUNT(*)
+FROM accounts
+WHERE active;
+
+CREATE OR REPLACE PROCEDURE account_insert(quantity int)
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    FOR i IN 0..quantity
+        LOOP
+            INSERT INTO accounts VALUES (true, i, 'savings');
+            IF i % 2 = 0 THEN
+                COMMIT;
+            ELSE
+                ROLLBACK;
+            END IF;
+        END LOOP;
+END;
+$$;
+
+CALL account_insert(100);
+
+SELECT COUNT(*)
+FROM accounts
+WHERE active;
